94123ad1a5a2bf09c3010e8cece359ffade32daa
Philip Bove <pgbson@gmail.com>
2016-05-29 00:47:48 -0400

    Added linux execution support

--- ftpclass.py
+++ ftpclass.py
@@ -5,13 +5,12 @@ import os
 class FTPclass():
     def __init__(self):
         self.ini = INI()
-
-    def DLWS(self, scripts, directory):  # downloads for windows execution
+    def DLLN(self,scripts,directorylinx):
         ip = self.ini.Config('ip')
         name = self.ini.Config('name')
         ftp = FTP(ip)
         ftp.login(name)
-        os.chdir(directory)
+        os.chdir(directorylinx)
         count = 0
         access = 0
         for v in scripts.values():  # iterates through number of dependencies needed
@@ -19,14 +18,35 @@ class FTPclass():
             value = scripts[str(access)]  # dls them based off of the order in ini file
             size = ftp.size(value)
             ftp.retrbinary('RETR ' + value, open(value, 'wb').write)
-            if os.path.exists(directory + '\\' + value):
-                while os.path.getsize(directory + '\\' + value) != size:  # checks to make sure file size is correct, if it is not correct this would indicate a bad download
+            if os.path.exists(directorylinx + '//' + value):
+                while os.path.getsize(directorylinx + '\\' + value) != size:  # checks to make sure file size is correct, if it is not correct this would indicate a bad download
+                    continue
+                sp.Popen([directorylinx+'//'+value])
+            else:
+                print('Download did not start for ' + value)
+            count = count + 1
+
+    def DLWS(self, scripts, directorywin):  # downloads for windows execution
+        ip = self.ini.Config('ip')
+        name = self.ini.Config('name')
+        ftp = FTP(ip)
+        ftp.login(name)
+        os.chdir(directorywin)
+        count = 0
+        access = 0
+        for v in scripts.values():  # iterates through number of dependencies needed
+            access = access + 1
+            value = scripts[str(access)]  # dls them based off of the order in ini file
+            size = ftp.size(value)
+            ftp.retrbinary('RETR ' + value, open(value, 'wb').write)
+            if os.path.exists(directorywin + '\\' + value):
+                while os.path.getsize(directorywin + '\\' + value) != size:  # checks to make sure file size is correct, if it is not correct this would indicate a bad download
                     continue
                 if '.ps1' in value:
                     powershell = sp.Popen([r'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe', '-ExecutionPolicy', 'Unrestricted','' + directory + '\\' + value])  #currently works, issue with powershell admin elevation but that is more of an issue with my powrshell script than this program
                     powershell.wait()
-                elif '.exe' in value:
-                     sp.Popen([directory+'\\'+value])
+                elif '.exe' or '.msi' in value:
+                     sp.Popen([directorywin+'\\'+value])
 
             else:
                 print('Download did not start for ' + value)
--- quickdeploy.py
+++ quickdeploy.py
@@ -6,24 +6,36 @@ import logging
 import datetime
 import ftplib
 import os
+import platform
 
 #TODO set up FTP_TLS support for external network use
 def main():
     ini = INI()
     sections = ini.CollectScripts()
     sections = sections
-    directory = 'C:\\Users\\' + getpass.getuser() + '\\desktop'
+    directorylinx = '//home//'+ getpass.getuser()+'//Desktop'
+    directorywin = 'C:\\Users\\' + getpass.getuser() + '\\desktop'
     UserAns = input("Hello, what do you need? > ")
-    check(UserAns, directory, sections,ini)
+    check(UserAns, directorywin, sections,ini,directorylinx)
 
 
-def check(UserAns, directory, sections,ini):
+def check(UserAns, directorywin, sections,ini,directorylinx):
     if UserAns in sections:
         ftp = FTPclass()
         package = UserAns  # package of scripts
         scripts = ini.Package(package)
         scripts = dict(scripts) #passes it to dict to be searched easier
-        ftp.DLWS(scripts, directory)
+        if platform.system() == 'Windows':
+            if '.exe' or '.ps1' or '.msi': #Check for compatability
+                ftp.DLWS(scripts, directorywin)
+            else:
+                print('This package contains Linux executables and will not run on Windows')
+        elif platform.system() =='Linux':
+            if '.exe' or '.ps1' or '.msi' not in scripts.values():
+                ftp.DLLN(scripts,directorylinx)
+            else:
+                print('This package contains Windows executables and will not run on Linux')
+
         main()
 
     elif UserAns.lower() == 'list':
